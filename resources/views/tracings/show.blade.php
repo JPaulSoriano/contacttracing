b3f548bfcf358f10f042b36de9a953d10603d9af
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-06-28 16:56:13 +0800

    062821

--- resources/views/tracings/show.blade.php
+++ resources/views/tracings/show.blade.php
@@ -9,16 +9,16 @@
                 <div class="card border-primary mb-3">
                 <div class="card-header border-0 bg-primary"><div class="text-white mb-0 text-uppercase h2 font-weight-bold text-center">{{$tracing->full_name}}</div></div>
                 <div class="card-body text-primary">
-                    <div class="text-primary"><i class="bi bi-person-fill"></i> {{$tracing->age}}</div>
-                    <div class="text-primary"><i class="bi bi-at"></i> {{$tracing->email}}</div>
-                    <div class="text-primary"><i class="bi bi-telephone-fill"></i> {{$tracing->phone}}</div>
-                    <div class="text-primary"><i class="bi bi-geo-alt-fill"></i> {{$tracing->street}} {{$tracing->city}} {{$tracing->province}}</div>
-                    <div class="text-primary"><i class="bi bi-book-fill"></i> {{$tracing->course}}</div>
-                    <div class="text-primary"><i class="bi bi-shield-fill"></i> {{$tracing->stud_type}}</div>
-                    <div class="text-primary"><i class="bi bi-person-fill"></i> {{$tracing->created_at}}</div>
-                    <div class="text-primary"><i class="bi bi-person-fill"></i> {{$tracing->est_date}}</div>
-                    <div class="text-primary"><i class="bi bi-person-fill"></i> {{ $tracing->timevisit->last()->created_at }}</div>
-                    <div class="text-primary"><i class="bi bi-layout-text-sidebar"></i> 
+                    <div class="text-primary">Age: {{$tracing->age}}</div>
+                    <div class="text-primary">Email: {{$tracing->email}}</div>
+                    <div class="text-primary">Phone: {{$tracing->phone}}</div>
+                    <div class="text-primary">Address: {{$tracing->street}} {{$tracing->city}} {{$tracing->province}}</div>
+                    <div class="text-primary">Course: {{$tracing->course}}</div>
+                    <div class="text-primary">Type: {{$tracing->stud_type}}</div>
+                    <div class="text-primary">Registered at: {{$tracing->created_at}}</div>
+                    <div class="text-primary">Estimated Arrival: {{$tracing->est_date}}</div>
+                    <div class="text-primary">Arrive at: {{ $tracing->timevisit->last()->created_at }}</div>
+                    <div class="text-primary">Purpose:
                         @foreach($tracing->purpose as $value)
                         {{$value}},
                         @endforeach
