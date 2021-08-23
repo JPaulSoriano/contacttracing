42810aa79ab9ad35cadb72bb41ac3c373652db2f
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-08-23 14:18:10 +0800

    scanner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,7 +106,25 @@
 </script>
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
-@yield('scripts')
+<script>
+        let scanner = new Instascan.Scanner(
+            {
+                video: document.getElementById('preview')
+            }
+        );
+        scanner.addListener('scan', function(content) {
+            alert('Escaneou o conteudo: ' + content);
+            window.open(content, "_blank");
+        });
+        Instascan.Camera.getCameras().then(cameras => 
+        {
+            if(cameras.length > 0){
+                scanner.start(cameras[0]);
+            } else {
+                console.error("Não existe câmera no dispositivo!");
+            }
+        });
+    </script>
 
 </body>
 </html>
--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -1,24 +1,9 @@
 @extends('layouts.app')
 @section('head')
-<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+<script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" ></script>	
 @endsection
 @section('content')
 <video id="preview"></video>
 @endsection
 @section('scripts')
-<script type="text/javascript">
-      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-      scanner.addListener('scan', function (content) {
-        alert(content);
-      });
-      Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-          scanner.start(cameras[0]);
-        } else {
-          console.error('No cameras found.');
-        }
-      }).catch(function (e) {
-        console.error(e);
-      });
-    </script>
 @endsection
