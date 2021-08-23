3b62550ba55dd1dc7fc461ab31bdcf75c0b982b2
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-08-23 13:41:59 +0800

    scanner

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::illustrated-layout')
+
+@section('title', __('Service Unavailable'))
+@section('code', '503')
+@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+@section('message', __($exception->getMessage() ?: 'Please come back later'))
