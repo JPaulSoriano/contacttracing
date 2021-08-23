3343101327e754e72b2b5c73bd078b3747756d08
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-08-23 14:10:59 +0800

    scanner

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -1,33 +1,56 @@
 @extends('layouts.app')
 @section('head')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/instascan/2.0.0-rc.4/instascan.min.js" integrity="sha512-vybWo2QCh2P1jTLx7W50N3K08p8ed7VsDZDJ9Ro/gvBDG0+lusOVFwbA9zfgBOtndpDm8YYKiagvre3Fq43kSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @endsection
 @section('content')
-<video id="preview" style="width:100%; height:275px;border:1px solid #CCC; margin-bottom: 15px"></video>
+
+<div id="box" class="card text-center">
+    <div class="card-header">
+      <video id="preview"></video>
+
+      </div>
+    <div class="card-body">
+      <div class="info">
+
+
+        <button id="res" type="button" class="btn btn-lg btn-lg btn-block btn-warning" disabled>
+          <div class="scanresult">
+            Scan Result !
+          </div>
+        </button>
+
+        <button id="confirm" style="display:none;" type="button" class="btn btn-warning  btn-lg btn-block">
+          <h1>Confirm</h1>
+        </button>
+      </div>
+
+</div>
 @endsection
 @section('scripts')
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-
-
 <script type="text/javascript">
-      let scanner = new Instascan.Scanner({ video: document.getElementById('preview'),mirror: false });
+      var audioElement = document.createElement('audio');
+      audioElement.setAttribute('src', 'assets/audio/beep.mp3');
+
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
-          console.log(content);
-		  <!-- $("#_qr_content").html(content); -->
-		  document.getElementById("_qr_content").innerHTML = content;
-          alert("QR GET!");
+        console.log(content);
+        audioElement.play();
+        $(".scanresult").text(content);
+        $("#confirm").css("display", "unset");
       });
       Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 1) {
+        if (cameras.length > 0) {
           scanner.start(cameras[1]);
+          vid();
         } else {
-          <!-- console.error('No cameras found.'); -->
-		  scanner.start(cameras[0]);
+          console.error('No cameras found.');
         }
       }).catch(function (e) {
         console.error(e);
       });
+
+      function vid() {
+        $("#preview").css("transform", "scaleX(1)");
+      }
 </script>
 @endsection
