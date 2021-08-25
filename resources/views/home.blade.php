eb5df8e07e6abeed19344ad0ead09db1f281f1b9
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-08-25 13:03:22 +0800

    reports

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,19 +14,19 @@
 
     <div class="row justify-content-center">
         <div class="col-lg-4">
-            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('approved') }}">Approved </br> {{$approvecount}}</a>
+            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('approved') }}">Approved </br> <span class="h3 text-info">{{$approvecount}}</span></a>
         </div>
         <div class="col-lg-4">
-            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('unapproved') }}">Unapproved </br> {{$unapprovecount}}</a>
+            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('unapproved') }}">Unapproved </br> <span class="h3 text-info">{{$unapprovecount}}</span></a>
         </div>
         <div class="col-lg-4">
-            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('staffindex') }}">All Appointments </br> {{$all}}</a>
+            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('staffindex') }}">All Appointments </br> <span class="h3 text-info">{{$all}}</span></a>
         </div>
         <div class="col-lg-4">
-            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('tomorrow') }}">Proposed to go tomorrow </br> {{$tomorrow}}</a>
+            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('tomorrow') }}">Proposed to go tomorrow </br> <span class="h3 text-info">{{$tomorrow}}</span></a>
         </div>
         <div class="col-lg-4">
-            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('today') }}">Proposed to go today </br> {{$today}}</a>
+            <a class="btn btn-sm btn-block btn-primary p-5 my-3" href="{{ route('today') }}">Proposed to go today </br> <span class="h3 text-info">{{$today}}</span></a>
         </div>
     </div>
 
